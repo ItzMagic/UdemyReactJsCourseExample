24c6e875a917bd963017ffd4c6f512273c348f15
Evan Londono <evanlondono@FLCPLC02WW49TJG5H.local>
2019-01-09 10:01:12 -0500

    fixed app to work from the errors I was having

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react';
 
+import classes from './Person.css';
 
-const person = (props) => {
-    
-    const rnd = Math.random();
-    
-    if (rnd > 0.7) {
-        throw new Error ('Something went wrong');
+class Person extends Component {
+    constructor( props ) {
+        super( props );
+        console.log( '[Person.js] Inside Constructor', props );
     }
-    return( 
 
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old! </p>
-            <input type="text" onChange={props.changed}/>
-        </div>
-    )    
-};
+    componentWillMount () {
+        console.log( '[Person.js] Inside componentWillMount()' );
+    }
 
-export default person;
+    componentDidMount () {
+        console.log( '[Person.js] Inside componentDidMount()' );
+    }
+
+    render () {
+        console.log( '[Person.js] Inside render()' );
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
+}
+
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,61 +1,111 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-import Persons from '../Components/Persons/Persons';
-import Cockpit from '../Components/Cockpit/Cockpit.css'
-import classes from './App.css'
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
-
-
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max' , age:28},
-      {name: 'Manu' , age:29},
-      {name: 'Stephanie' , age:26}
-    ],
-    showPersons: false //Boolean
-  }
-
-  togglePersonsHandler = ( event ) => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
-  nameChangedHandler = (event) => {
-    this.setState( { 
+class App extends PureComponent {
+  constructor( props ) {
+    super( props );
+    console.log( '[App.js] Inside Constructor', props );
+    this.state = {
       persons: [
-        { name: 'Max', age: 28},
-        { name: event.target.value, age: 29},
-        { name: 'Stephanie',age: 26} 
-      ]  
-    } ) 
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasdf1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false
+    };
   }
-  switchNameHandler = () => { // this is a normal function aka method
-    // console.log('was clicked!')
-    this.setState( {persons: [ {name: 'Maximillan' , age:28}, {name: 'Manu' , age:29}, {name: 'Stephanie' , age:27} ]} )}
-    
-    
-    render() {
-      
-    let persons = null;
-    let btnClass= '';
 
-    if( this.state.showPersons ) {
-      persons = <Persons 
-            persons={this.state.persons}
-            clicked ={this.deletePersonHandler}
-            changed ={this.nameChangedHandler}/>;
+  componentWillMount () {
+    console.log( '[App.js] Inside componentWillMount()' );
+  }
+
+  componentDidMount () {
+    console.log( '[App.js] Inside componentDidMount()' );
+  }
+
+  // shouldComponentUpdate ( nextProps, nextState ) {
+  //   console.log( '[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState );
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  // }
+
+  componentWillUpdate ( nextProps, nextState ) {
+    console.log( '[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState );
+  }
+
+  componentDidUpdate () {
+    console.log( '[UPDATE App.js] Inside componentDidUpdate' );
+  }
+
+  // state = {
+  //   persons: [
+  //     { id: 'asfa1', name: 'Max', age: 28 },
+  //     { id: 'vasdf1', name: 'Manu', age: 29 },
+  //     { id: 'asdf11', name: 'Stephanie', age: 26 }
+  //   ],
+  //   otherState: 'some other value',
+  //   showPersons: false
+  // }
+
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    } );
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons } );
+  }
+
+  deletePersonHandler = ( personIndex ) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    console.log( '[App.js] Inside render()' );
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.deletePersonHandler}
+        changed={this.nameChangedHandler} />;
     }
 
     return (
       <div className={classes.App}>
-        <Cockpit showPersons={this.state.showPersons}
-          persons = {this.state.persons}
-          clicked ={this.togglePersonsHandler}/>
+        <button onClick={() => { this.setState( { showPersons: true } ) }}>Show Persons</button>
+        <Cockpit
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler} />
         {persons}
       </div>
-
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default    App;
+export default App;
